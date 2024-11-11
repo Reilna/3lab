f882a267dce422cdbdb2bab5605c3cdd358229ad
keil <85560517+Reilna@users.noreply.github.com>
2024-11-11 12:40:19 +0300

    Add files via upload

---
+++ lab3/mathutils/mathutils.go
@@ -0,0 +1,25 @@
+package mathutils
+
+import "fmt"
+
+//task_1
+func Fact() {
+
+	fmt.Print("\ntask_1\nEnter number: ")
+	var a int64
+	sum := int64(1)
+
+	fmt.Scan(&a)
+
+	if a == 0 {
+		fmt.Print(1)
+	} else if a > 0 {
+		for b := int64(1); b <= a; b++ {
+			sum *= b
+		}
+		fmt.Print("fact ", a, " - ", sum)
+	} else {
+		fmt.Print("Enter the positive number")
+	}
+
+}
